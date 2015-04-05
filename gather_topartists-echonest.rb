715ba197795508609cf9a235146a8d4315416ac7
Ben Lieberman <blieberman@brandywine.net>
2015-04-05 22:32:43 +0000

    implementing mongodb inserts for top artist collection

--- gather_topartists-echonest.rb
+++ gather_topartists-echonest.rb
@@ -1,13 +1,23 @@
 #!/usr/local/rvm/rubies/ruby-2.1.0/bin/ruby
 
 require "uri"
-require "net/http"
 require "json"
+require "net/http"
+require "mongo"
+include Mongo
 
+## ECHONEST API KEYS ##
 API_KEY = 'L7IXI5E2ZRRIRQDRD'
 CONSUMER_KEY = '14b2e4475de5e5be91631c47f20839c9'
 SHARED_SECRET = '4Es+qT0HS6GOce7t1F2JCA'
+####
 
+
+# open mongo connection
+db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'music')
+documents = db[:artists]
+
+# prep http request
 URL = "http://developer.echonest.com/api/v4/artist/top_hottt?api_key=#{ API_KEY }&format=json&results=500&start=0&bucket=hotttnesss"
 uri = URI.parse(URL)
 
@@ -19,7 +29,5 @@ response = http.request(request)
 result = JSON.parse(response.body)
 
 result['response']['artists'].each do |artist|
-  puts artist['name']
-  puts artist['hotttnesss']
-  puts
+  documents.insert_one({"name" => artist['name'], "twitterHandle" => "", "popularity" => artist['hotttnesss']});
 end
