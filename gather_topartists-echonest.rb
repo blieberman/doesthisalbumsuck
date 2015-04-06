f3b1314cbecc69483cded40bc12680e493faf7e2
Ben Lieberman <blieberman@brandywine.net>
2015-04-06 01:56:28 +0000

    implementing echonest twitter handle api calls

--- gather_topartists-echonest.rb
+++ gather_topartists-echonest.rb
@@ -12,22 +12,49 @@ CONSUMER_KEY = '14b2e4475de5e5be91631c47f20839c9'
 SHARED_SECRET = '4Es+qT0HS6GOce7t1F2JCA'
 ####
 
-
 # open mongo connection
 db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'music')
 documents = db[:artists]
 
-# prep http request
-URL = "http://developer.echonest.com/api/v4/artist/top_hottt?api_key=#{ API_KEY }&format=json&results=500&start=0&bucket=hotttnesss"
-uri = URI.parse(URL)
+# prep top aritsts http request
+TOP_ARTISTS_URL = "http://developer.echonest.com/api/v4/artist/top_hottt?api_key=#{ API_KEY }&format=json&results=300&start=0&bucket=hotttnesss"
+topArtist_uri = URI.parse(TOP_ARTISTS_URL)
 
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
+topArtist_http = Net::HTTP.new(topArtist_uri.host, topArtist_uri.port)
+topArtist_request = Net::HTTP::Get.new(topArtist_uri.request_uri)
 
-response = http.request(request)
+topArtist_response = topArtist_http.request(topArtist_request)
+topArtist_result = JSON.parse(topArtist_response.body)
 
-result = JSON.parse(response.body)
+callCount = 0
 
-result['response']['artists'].each do |artist|
-  documents.insert_one({"name" => artist['name'], "twitterHandle" => "", "popularity" => artist['hotttnesss']});
+topArtist_result['response']['artists'].each do |artist|
+  artistName = artist['name']
+  puts artistName
+  aNPrepped = artistName.gsub! '&', 'and'
+  aNPrepped = URI.escape(artistName)
+  puts aNPrepped
+
+  # prep twitter handle http request
+  twitter_handle_url = "http://developer.echonest.com/api/v4/artist/twitter?api_key=#{ API_KEY }&name=#{ aNPrepped }&format=json"
+  twitterHandle_uri = URI.parse(twitter_handle_url)
+
+  twitterHandle_http = Net::HTTP.new(twitterHandle_uri.host, twitterHandle_uri.port)
+  twitterHandle_request = Net::HTTP::Get.new(twitterHandle_uri.request_uri)
+
+  twitterHandle_response = twitterHandle_http.request(twitterHandle_request)
+  twitterHandle_result = JSON.parse(twitterHandle_response.body)
+
+  twitterHandle = ""
+  twitterHandle = twitterHandle_result['response']['artist']['twitter']
+  puts twitterHandle
+
+  documents.insert_one({"name" => artistName, "twitterHandle" => twitterHandle, "popularity" => artist['hotttnesss']});
+  puts callCount
+  callCount += 1
+  if callCount >= 115
+    puts "SLEEPING TO AVOID RATE LIMIT..."
+    sleep 20
+    callCount = 0
+  end
 end
